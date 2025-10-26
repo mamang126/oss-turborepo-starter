676cb669f4cb3d96824973cb1f872f57d97f812c
Miguel A. <mamang126@gmail.com>
2025-10-26 19:32:16 +0100

    feat: Tailwind & React & Shadcn

--- apps/app/astro.config.mjs
+++ apps/app/astro.config.mjs
@@ -1,7 +1,16 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
